65fb34c4d79ba49cc7ae418f14386f8cc6155f13
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-05-24 11:28:01 +0300

    Update project

--- js/script.js
+++ js/script.js
@@ -10,17 +10,17 @@ function anichange(objName) {
   }
 }
 
-$('.notification-info-button-1').click(function () {
-  anichange('#notification-text-1');
+$('.question-and-answer-1').click(function () {
+  anichange('#question-and-answer-text-1');
   return false;
 })
 
-$('.notification-info-button-2').click(function () {
-  anichange('#notification-text-2');
+$('.question-and-answer-2').click(function () {
+  anichange('#question-and-answer-text-2');
   return false;
 })
 
-$('.notification-info-button-3').click(function () {
-  anichange('#notification-text-3');
+$('.question-and-answer-3').click(function () {
+  anichange('#question-and-answer-text-3');
   return false;
 })
